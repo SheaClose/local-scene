6d518aa339448a6e71bc5bf74f99310e61f45dc3
nicole pank <nicole.pank@gmail.com>
2013-11-20 01:47:07 -0600

    initial commit

---
+++ scene_tracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SceneTracker::Application.config.secret_key_base = 'fc4b8a0a07a94ae2479cb80877b0f5879ed71ff40fde9bb265d7e0c91d0eadecef93ad3e74fe9dfb4bedccbac48fcc502f076173bb6f0e2fe12093b52853989d'
---
+++ scene_tracker/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SceneTracker::Application.config.session_store :cookie_store, key: '_scene_tracker_session'
